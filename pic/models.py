ab78c320df4cb7b3537e6c31628a45339a63ce07
jkingportfolio <mycodejourney3@gmail.com>
2022-12-05 19:59:37 +0000

    Add: Post and Tag models to models.py

--- pic/models.py
+++ pic/models.py
@@ -1,24 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.db.models.signals import post_save, post_delete
+import uuid
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+# uploading user files to a specific directory
 
-# class Post(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # picture = CloudinaryField('image', default='placeholder')
-    # caption = models.CharField(max_length=1000, verbose_name="Caption")
-    # posted = models.DateField(auto_now_add=True)
-    # tags = models.ManyToManyField(Tag, related_name="tags")
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # likes = models.IntegerField(default=0)
 
-    # class Meta:
-    #     ordering = ["-posted"]
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
-    # def __str__(self):
-    #     return self.title
 
-    # def number_of_likes(self):
-    #     return self.likes.count()
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Tag')
+    slug = models.SlugField(null=False, unique=True, default=uuid.uuid1)
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def get_absolute_url(self):
+        return reverse('tags', args=[self.slug])
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    picture = CloudinaryField('image', default='placeholder')
+    caption = models.CharField(max_length=10000, verbose_name="Caption")
+    posted = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="tags")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse("post-details", args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
