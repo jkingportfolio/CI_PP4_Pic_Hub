16c56f8be5d02c5758ec29c2a655cd966a7f0a9d
jkingportfolio <mycodejourney3@gmail.com>
2022-12-05 18:14:10 +0000

    Add: index view,urls.py and url path

--- pic/models.py
+++ pic/models.py
@@ -5,20 +5,20 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 
 
-class Post(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    picture = CloudinaryField('image', default='placeholder')
-    caption = models.CharField(max_length=1000, verbose_name="Caption")
-    posted = models.DateField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag, related_name="tags")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.IntegerField(default=0)
+# class Post(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # picture = CloudinaryField('image', default='placeholder')
+    # caption = models.CharField(max_length=1000, verbose_name="Caption")
+    # posted = models.DateField(auto_now_add=True)
+    # tags = models.ManyToManyField(Tag, related_name="tags")
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # likes = models.IntegerField(default=0)
 
-    class Meta:
-        ordering = ["-posted"]
+    # class Meta:
+    #     ordering = ["-posted"]
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
 
-    def number_of_likes(self):
-        return self.likes.count()
+    # def number_of_likes(self):
+    #     return self.likes.count()
