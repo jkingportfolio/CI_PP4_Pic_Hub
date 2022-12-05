1cdf9816b3a9f5935fed9272990a88141e4bf0a4
jkingportfolio <mycodejourney3@gmail.com>
2022-12-05 20:44:26 +0000

    Add: Register Likes,Follow and Feed models to admin.py

--- pic/admin.py
+++ pic/admin.py
@@ -4,3 +4,6 @@ from pic.models import Tag, Post
 # Register your models here.
 admin.site.register(Tag)
 admin.site.register(Post)
+admin.site.register(Likes)
+admin.site.register(Follow)
+admin.site.register(Feed)
