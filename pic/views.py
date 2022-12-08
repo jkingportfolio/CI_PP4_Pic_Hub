b8d48e4e16fa88b01778e77bc801b0862ea8fd14
jkingportfolio <mycodejourney3@gmail.com>
2022-12-08 19:09:39 +0000

    Add: Render post fields to index.html block

--- pic/views.py
+++ pic/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-from post.models import Tag, Post, Likes, Follow, Feed
+from django.contrib.auth.models import User
+from pic.models import Tag, Post, Likes, Follow, Feed
 from django.contrib.auth.decorators import login_required
 
+
 def index(request):
-    user = request.user
+    user = User.objects.get(username=request.user.username)
     feed_posts = Feed.objects.filter(user=user)
     feed_posts_list = []
     for post in feed_posts:
         feed_posts_list.append(post.post_id)
-    render_posts = Post.objects.filter(id__in=feed_posts_list).all().order_by('-posted')
-    content = {
-        'posts' : render_posts 
+    render_posts = Post.objects.filter(
+        id__in=feed_posts_list).all().order_by('-posted')
+    context = {
+        'render_posts': render_posts,
     }
 
-    return render(request, 'index.html',)
-
-
+    return render(request, 'index.html', context)
