3037e82850d6bc7caf84f0f365578b485efe8110
jkingportfolio <mycodejourney3@gmail.com>
2022-12-08 16:51:01 +0000

    Update: index view with post details

--- pic/views.py
+++ pic/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from post.models import Tag, Post, Likes, Follow, Feed
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return render(request, 'index.html')
+    user = request.user
+    feed_posts = Feed.objects.filter(user=user)
+    feed_posts_list = []
+    for post in feed_posts:
+        feed_posts_list.append(post.post_id)
+    render_posts = Post.objects.filter(id__in=feed_posts_list).all().order_by('-posted')
+    content = {
+        'posts' : render_posts 
+    }
+
+    return render(request, 'index.html',)
+
+
